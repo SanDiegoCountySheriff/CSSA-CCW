b21bd4502c12a7650b253d457a31ac08697c177b
jeremiahbooth <jboothwebdev@gmail.com>
2022-09-28 13:20:45 -0700

    Created test for the implementation components.

---
+++ UI/libs/core-public/tests/unit/components/containers/FormButtonContainer.spec.ts
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import FormButtonContainer from '@core-public/components/containers/FormButtonContainer.vue';
+import Vuetify from 'vuetify';
+import { createLocalVue, mount } from '@vue/test-utils';
+
+Vue.use(Vuetify);
+const localVue = createLocalVue();
+const tMock = {
+  $t: text => text,
+};
+
+describe('FormButtonContainer', () => {
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    //@ts-ignore
+    wrapper = mount(FormButtonContainer, {
+      localVue,
+      vuetify,
+      mocks: tMock,
+    });
+  });
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it('should render all three buttons', () => {
+    expect(wrapper.findAll('button').length).toBe(3);
+  });
+
+  it('Should emit on button press', async () => {
+    await wrapper.setData({ valid: true });
+    const button = wrapper.findAll('button').at(0);
+    button.trigger('click');
+    expect(wrapper.emitted().submit).toHaveLength(1);
+  });
+});
---
+++ UI/libs/shared/ui/tests/unit/components/AliasTable.spec.ts
@@ -0,0 +1,103 @@
+import Vue from 'vue';
+import AliasTable from '@shared-ui/components/tables/AliasTable.vue';
+import Vuetify from 'vuetify';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+
+Vue.use(Vuetify);
+const localVue = createLocalVue();
+const alias = {
+  prevLastName: 'pLast',
+  prevFirstName: 'pFirst',
+  prevMiddleName: 'pMiddle',
+  cityWhereChanged: 'city',
+  stateWhereChanged: 'state',
+  courtFileNumber: 'court',
+};
+
+describe('AliasTable', () => {
+  let vuetify;
+  const tMock = {
+    $t: text => text,
+  };
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  it('Should render the Alias table with the correct text in the first row', () => {
+    //@ts-ignore
+    const wrapper = shallowMount(AliasTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        aliases: [alias],
+      },
+      mocks: tMock,
+    });
+    expect(wrapper.findAll('td').at(0).element.textContent).toContain('pLast');
+  });
+
+  it('Should render the Alias table with the correct text in the second row', () => {
+    //@ts-ignore
+    const wrapper = shallowMount(AliasTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        aliases: [alias],
+      },
+      mocks: tMock,
+    });
+    expect(wrapper.findAll('td').at(1).element.textContent).toContain('pFirst');
+  });
+
+  it('Should render the Alias table with the correct text in the third row', () => {
+    //@ts-ignore
+    const wrapper = shallowMount(AliasTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        aliases: [alias],
+      },
+      mocks: tMock,
+    });
+    expect(wrapper.findAll('td').at(2).element.textContent).toContain(
+      'pMiddle'
+    );
+  });
+  it('Should render the Alias table with the correct text in the fourth row', () => {
+    //@ts-ignore
+    const wrapper = shallowMount(AliasTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        aliases: [alias],
+      },
+      mocks: tMock,
+    });
+    expect(wrapper.findAll('td').at(3).element.textContent).toContain('city');
+  });
+  it('Should render the Alias table with the correct text in the fifth row', () => {
+    //@ts-ignore
+    const wrapper = shallowMount(AliasTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        aliases: [alias],
+      },
+      mocks: tMock,
+    });
+    expect(wrapper.findAll('td').at(4).element.textContent).toContain('state');
+  });
+  it('Should render the Alias table with the correct text in the fifth row', () => {
+    //@ts-ignore
+    const wrapper = shallowMount(AliasTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        aliases: [alias],
+      },
+      mocks: tMock,
+    });
+    expect(wrapper.findAll('td').at(5).element.textContent).toContain('court');
+  });
+});
