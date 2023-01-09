5609206d591a123b25f51ae677bcadd4276c0ecb
Jake Kellas <jacob.kellas@sdsheriff.org>
2023-01-09 13:34:59 -0800

    feat: update side-bar

--- UI/libs/core-admin/src/lib/components/navigation/SideBar.vue
+++ UI/libs/core-admin/src/lib/components/navigation/SideBar.vue
@@ -5,24 +5,13 @@
     app
     v-model="drawer"
     :mini-variant.sync="mini"
-    class="sidebar"
-    permanent
-    floating
   >
-    <v-list
-      class="mt-1"
-      nav
-    >
-      <v-list-item
-        class="px-2 logo"
-        :to="Routes.HOME_ROUTE_PATH"
-        link
-        style="background: none"
-      >
+    <v-list nav>
+      <v-list-item :to="Routes.HOME_ROUTE_PATH">
         <v-list-item-avatar
           height="32"
           width="32"
-          color="black"
+          class="mx-0 px-0"
         >
           <v-skeleton-loader
             v-if="isLoading"
@@ -40,120 +29,114 @@
           />
         </v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title class="font-weight-medium">
-            {{ getAppTitle.name }} <small> {{ getAppTitle.env }} </small>
+          <v-list-item-title>
+            {{ getAppTitle.name }}
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
-      <v-card class="mt-6 mb-6 elevation-0">
-        <v-list-item>
-          <SearchBar />
+      <v-list-item>
+        <SearchBar />
+      </v-list-item>
+
+      <v-list dense>
+        <v-list-item
+          :to="Routes.HOME_ROUTE_PATH"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="text-left">
+              {{ $t('Dashboard') }}
+            </v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-      </v-card>
 
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item style="display: none"></v-list-item>
-        <v-card class="mt-2 mb-2 elevation-0">
-          <v-list-item
-            :to="Routes.HOME_ROUTE_PATH"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-view-dashboard</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ $t('Dashboard') }}</v-list-item-title>
-          </v-list-item>
-        </v-card>
-        <v-card class="mt-2 mb-2 elevation-0">
-          <v-list-item
-            :to="Routes.APPOINTMENTS_ROUTE_PATH"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-calendar-blank-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              {{ $t('Appointments') }}
-              <v-chip
-                v-if="aptStore.getNewAptCount !== 0"
-                class="ml-5 font-weight-bold"
-                :color="$vuetify.theme.dark ? '' : 'light-blue lighten-4'"
-                x-small
-              >
-                {{ aptStore.getNewAptCount }}
-              </v-chip>
-            </v-list-item-title>
-          </v-list-item>
-        </v-card>
-        <v-card class="mt-2 mb-2 elevation-0">
-          <v-list-item
-            :to="Routes.PERMITS_ROUTE_PATH"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-file-document</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              {{ $t('Applications') }}
-              <v-chip
-                v-if="permitStore.getOpenPermits !== 0"
-                class="ml-8 font-weight-bold"
-                :color="$vuetify.theme.dark ? '' : 'light-blue lighten-4'"
-                x-small
-              >
-                {{ permitStore.getOpenPermits }}
-              </v-chip>
-            </v-list-item-title>
-          </v-list-item>
-        </v-card>
-        <v-card class="mt-2 mb-2 elevation-0">
-          <v-list-item
-            :to="Routes.RECEIPT_ROUTE_PATH"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-note-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ $t('Receipts') }}</v-list-item-title>
-          </v-list-item>
-        </v-card>
-        <v-card class="mt-2 mb-2 elevation-0">
-          <v-list-item
-            :to="Routes.SETTINGS_ROUTE_PATH"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-cog</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ $t('Admin Settings') }}</v-list-item-title>
-          </v-list-item>
-        </v-card>
-      </v-list>
+        <v-list-item
+          :to="Routes.APPOINTMENTS_ROUTE_PATH"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-calendar-blank-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-left">
+            {{ $t('Appointments') }}
+            <v-chip
+              v-if="aptStore.getNewAptCount !== 0"
+              class="ml-5 font-weight-bold"
+              :color="$vuetify.theme.dark ? '' : 'light-blue lighten-4'"
+              x-small
+            >
+              {{ aptStore.getNewAptCount }}
+            </v-chip>
+          </v-list-item-title>
+        </v-list-item>
 
-      <v-list
-        class="bottom-list"
-        nav
-        dense
-      >
-        <v-card class="mt-2 mb-2 elevation-0">
-          <v-list-item
-            link
-            @click="mini = !mini"
-          >
-            <v-list-item-icon>
-              <v-icon>
-                {{ mini ? 'mdi-menu-right-outline' : 'mdi-menu-left-outline' }}
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ $t('Collapse Menu') }}</v-list-item-title>
-          </v-list-item>
-        </v-card>
+        <v-list-item
+          :to="Routes.PERMITS_ROUTE_PATH"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-file-document</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-left">
+            {{ $t('Applications') }}
+            <v-chip
+              v-if="permitStore.getOpenPermits !== 0"
+              class="ml-8 font-weight-bold"
+              :color="$vuetify.theme.dark ? '' : 'light-blue lighten-4'"
+              x-small
+            >
+              {{ permitStore.getOpenPermits }}
+            </v-chip>
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item
+          :to="Routes.RECEIPT_ROUTE_PATH"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-note-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-left">
+            {{ $t('Receipts') }}
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item
+          :to="Routes.SETTINGS_ROUTE_PATH"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-left">
+            {{ $t('Admin Settings') }}
+          </v-list-item-title>
+        </v-list-item>
       </v-list>
     </v-list>
+
+    <template #append>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item @click="mini = !mini">
+          <v-list-item-icon>
+            <v-icon>
+              {{ mini ? 'mdi-menu-right-outline' : 'mdi-menu-left-outline' }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-left">
+            {{ $t('Collapse Menu') }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </template>
   </v-navigation-drawer>
 </template>
 
@@ -177,89 +160,3 @@ const { isLoading } = useQuery(['logo']);
 
 const getAppTitle = useEnvName();
 </script>
-
-<style lang="scss" scoped>
-.theme--light.sidebar {
-  background: #f5f5f5;
-}
-.sidebar {
-  max-width: 265px;
-
-  .v-avatar {
-    min-width: 32px !important;
-  }
-
-  .v-list--nav {
-    padding-left: 4px;
-    padding-right: 4px;
-  }
-
-  .theme--light .logo {
-    &::before {
-      opacity: 0 !important;
-    }
-
-    .v-list-item__title {
-      color: #344054;
-    }
-  }
-
-  .theme--light.v-navigation-drawer {
-    background-color: #f7f9fb !important;
-  }
-
-  .theme--dark {
-    .logo {
-      .v-list-item__title {
-        color: white;
-      }
-    }
-    .v-list-item {
-      &__title {
-        color: white;
-      }
-    }
-  }
-
-  .v-list .v-list-item--active .theme--light {
-    background: #eff8ff;
-    color: #2e90fa;
-
-    &__title {
-      color: #2e90fa;
-    }
-  }
-
-  .v-list-item {
-    height: 46px;
-
-    &__icon {
-      margin-top: 10px;
-      min-width: 20px;
-    }
-
-    &__title {
-      text-align: left;
-      font-size: 16px;
-      font-weight: 400;
-      line-height: 26px;
-      color: #667085;
-    }
-
-    &__subtitle {
-      text-align: left;
-    }
-
-    &:not(:last-child):not(:only-child) {
-      margin-bottom: 8px;
-    }
-  }
-  .bottom-list {
-    .v-card {
-      position: absolute;
-      bottom: 20px;
-      width: 240px;
-    }
-  }
-}
-</style>
