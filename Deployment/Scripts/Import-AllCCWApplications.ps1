f5ddab5b20383ea7c914fc9ed2a9767e764dc928
lmcwhirt-insight <86177157+lmcwhirt-insight@users.noreply.github.com>
2023-01-31 10:28:21 -0700

    Removing invalid parameter value 'true'

--- Deployment/Scripts/Import-AllCCWApplications.ps1
+++ Deployment/Scripts/Import-AllCCWApplications.ps1
@@ -1,5 +1,5 @@
 Write-Host "Removing existing Az module"
-Uninstall-Module Az -AllVersions true -Force 
+Uninstall-Module Az -AllVersions -Force 
 
 Write-Host "Installing Az v6.6.0 due to Az.Account version in image"
 Install-Module Az -Repository PSGallery -RequiredVersion 6.6.0 -AllowClobber -Force
