701ede029de6293458a941883d0292169ae93a7f
nwolfosh <104584843+nwolfosh@users.noreply.github.com>
2023-08-23 10:43:18 -0700

    Task: Refactor application creation logic
    - Remove immediate application creation when button is clicked
    - Added application setup logic into final acknowledgement step

--- UI/libs/core-public/src/lib/views/HomeView.vue
+++ UI/libs/core-public/src/lib/views/HomeView.vue
@@ -37,7 +37,6 @@
               v-else-if="
                 authStore.getAuthState.isAuthenticated && data?.length === 0
               "
-              :loading="isLoading"
               @click="createNewApplication"
               color="primary"
               x-large
@@ -84,13 +83,12 @@ import Loader from '@core-public/views/Loader.vue'
 import { MsalBrowser } from '@shared-ui/api/auth/authentication'
 import PriceInfoWrapper from '@core-public/components/wrappers/PriceInfoWrapper.vue'
 import Routes from '@core-public/router/routes'
-import { defaultPermitState } from '@shared-utils/lists/defaultConstants'
 import { useAuthStore } from '@shared-ui/stores/auth'
 import { useBrandStore } from '@shared-ui/stores/brandStore'
 import { useCompleteApplicationStore } from '@shared-ui/stores/completeApplication'
+import { useQuery } from '@tanstack/vue-query'
 import { useRouter } from 'vue-router/composables'
 import { inject, ref } from 'vue'
-import { useMutation, useQuery } from '@tanstack/vue-query'
 
 const brandStore = useBrandStore()
 const authStore = useAuthStore()
@@ -107,48 +105,12 @@ const { data, isFetching } = useQuery(
   }
 )
 
-const { isLoading, mutate: createMutation } = useMutation({
-  mutationFn: completeApplicationStore.createApplication,
-  onSuccess: () => {
-    router.push({
-      path: Routes.APPLICATION_ROUTE_PATH,
-      query: {
-        applicationId: completeApplicationStore.completeApplication.id,
-        isComplete:
-          completeApplicationStore.completeApplication.application.isComplete.toString(),
-      },
-    })
-  },
-  onError: () => null,
-})
-
 function handleLogIn() {
   msalInstance.value.logIn()
 }
 
 function createNewApplication() {
-  completeApplicationStore.setCompleteApplication(defaultPermitState)
-  completeApplicationStore.completeApplication.application.cost = {
-    new: {
-      standard: brandStore.brand.cost.new.standard,
-      judicial: brandStore.brand.cost.new.judicial,
-      reserve: brandStore.brand.cost.new.reserve,
-    },
-    renew: {
-      standard: brandStore.brand.cost.renew.standard,
-      judicial: brandStore.brand.cost.renew.judicial,
-      reserve: brandStore.brand.cost.renew.reserve,
-    },
-    issuance: brandStore.brand.cost.issuance,
-    modify: brandStore.brand.cost.modify,
-    creditFee: brandStore.brand.cost.creditFee,
-    convenienceFee: brandStore.brand.cost.convenienceFee,
-  }
-  completeApplicationStore.completeApplication.application.userEmail =
-    authStore.auth.userEmail
-  completeApplicationStore.completeApplication.id = window.crypto.randomUUID()
-  completeApplicationStore.completeApplication.application.currentStep = 0
-  createMutation()
+  router.push({ path: Routes.APPLICATION_ROUTE_PATH })
 }
 
 function viewApplication() {
