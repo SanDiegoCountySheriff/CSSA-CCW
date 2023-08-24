0f6061f82e3bbd93f29cc50368c316d2ee931cb9
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2023-08-24 14:19:36 -0700

    feat: improve file upload step

--- API/CCW.Application/Entities/Agreements.cs
+++ API/CCW.Application/Entities/Agreements.cs
@@ -1,16 +1,12 @@
 namespace CCW.Application.Entities
-{ 
+{
     public class Agreements
     {
         public bool GoodMoralCharacterAgreed { get; set; }
-        public string? GoodMoralCharacterAgreedDate { get; set; }
+        public string GoodMoralCharacterAgreedDate { get; set; }
         public bool ConditionsForIssuanceAgreed { get; set; }
-        public string? ConditionsForIssuanceAgreedDate { get; set; }
+        public string ConditionsForIssuanceAgreedDate { get; set; }
         public bool FalseInfoAgreed { get; set; }
-        public string? FalseInfoAgreedDate { get; set; }
-
+        public string FalseInfoAgreedDate { get; set; }
     }
-
 }
-
-
