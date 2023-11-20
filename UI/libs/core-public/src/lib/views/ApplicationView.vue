9e125fff3d738d515c0d7d21a4ec538eb10d127a
Jake Kellas <jacob.kellas@sdsheriff.org>
2023-11-20 13:16:24 -0800

    chore: code cleanup

--- UI/libs/core-public/src/lib/views/ApplicationView.vue
+++ UI/libs/core-public/src/lib/views/ApplicationView.vue
@@ -2,25 +2,11 @@
   <MoreInformationContainer
     :information-only="route.params.informationOnly === 'true'"
   />
-  <!-- <AcknowledgementContainer
-      :next-route="`${Routes.FORM_ROUTE_PATH}?orderId=${route.params}`"
-    /> -->
 </template>
 
 <script setup lang="ts">
-import AcknowledgementContainer from '@core-public/components/containers/AcknowledgementContainer.vue'
 import MoreInformationContainer from '@core-public/components/containers/MoreInformationContainer.vue'
-import Routes from '@core-public/router/routes'
-import { useBrandStore } from '@shared-ui/stores/brandStore'
-import { onMounted } from 'vue'
-import { useRoute, useRouter } from 'vue-router/composables'
+import { useRoute } from 'vue-router/composables'
 
-const store = useBrandStore()
 const route = useRoute()
-const router = useRouter()
-
-onMounted(() => {
-  window.console.log(route.params)
-  window.console.log(router.currentRoute)
-})
 </script>
