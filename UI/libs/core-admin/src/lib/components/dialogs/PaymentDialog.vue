a70adcf0436c3c16027bf5c3d505fd2946f7a1c8
Jake Kellas <jacob.kellas@sdsheriff.org>
2023-12-01 12:36:15 -0800

    feat: add temporary payment dialog

--- UI/libs/core-admin/src/lib/components/dialogs/PaymentDialog.vue
+++ UI/libs/core-admin/src/lib/components/dialogs/PaymentDialog.vue
@@ -90,3 +90,87 @@ const mostRecentPayment = computed(
   }
 }
 </style>
+
+
+<!-- <template>
+  <v-dialog
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+    v-model="state.dialog"
+  >
+    <template #activator="{ on, attrs }">
+      <v-btn
+        small
+        block
+        color="primary"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon left>mdi-currency-usd</v-icon>
+        {{ mostRecentPayment ? mostRecentPayment : $t('No payment history') }}
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-toolbar
+        color="primary"
+        dense
+        dark
+      >
+        <v-btn
+          icon
+          @click="state.dialog = false"
+        >
+          <v-icon> mdi-close </v-icon>
+        </v-btn>
+
+        <v-toolbar-title color="primary">
+          Financial Transactions
+        </v-toolbar-title>
+      </v-toolbar>
+
+      <v-card-title></v-card-title>
+
+      <v-card-text>
+        <v-row>
+          <v-col
+            cols="12"
+            lg="6"
+            md="6"
+          >
+            <PaymentHistory />
+          </v-col>
+
+          <v-col
+            cols="12"
+            lg="6"
+            md="6"
+          >
+            <ReceiptForm :update-payment="mostRecentPayment" />
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+import PaymentHistory from '@core-admin/components/receipt/PaymentHistory.vue'
+import ReceiptForm from '@core-admin/components/receipt/ReceiptForm.vue'
+import { usePermitsStore } from '@core-admin/stores/permitsStore'
+import { computed, reactive } from 'vue'
+
+const permitStore = usePermitsStore()
+
+const state = reactive({
+  dialog: false,
+})
+
+const mostRecentPayment = computed(
+  () =>
+    permitStore.getPermitDetail?.paymentHistory[
+      permitStore.getPermitDetail.paymentHistory.length - 1
+    ]?.paymentType
+)
+</script> -->
