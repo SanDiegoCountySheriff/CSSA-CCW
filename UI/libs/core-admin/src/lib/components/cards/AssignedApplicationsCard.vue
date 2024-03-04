5e8a23f7bcf1c116aedcb39b412e8cd1656fdc45
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2024-03-04 07:43:53 -0800

    feat: update assigned applications card (#590)

--- UI/libs/core-admin/src/lib/components/cards/AssignedApplicationsCard.vue
+++ UI/libs/core-admin/src/lib/components/cards/AssignedApplicationsCard.vue
@@ -1,61 +1,98 @@
 <template>
   <v-card v-if="authStore.getAuthState.isAuthenticated">
-    <v-card-title>My Assigned Applications</v-card-title>
+    <v-card-title>
+      My Assigned Applications
+      <v-spacer />
+      <v-text-field
+        v-model="search"
+        label="Search"
+        color="primary"
+        outlined
+        single-line
+        hide-details
+        dense
+      ></v-text-field>
+    </v-card-title>
 
     <v-card-text>
-      <template v-for="(permit, index) in assignedApplications">
-        <router-link
-          :key="index"
-          :to="{
-            name: 'PermitDetail',
-            params: { orderId: permit.orderId },
-          }"
-          tag="a"
-          target="_self"
-          style="text-decoration: none; color: inherit"
-          replace
-        >
-          <v-row>
-            <v-col>
-              {{ permit.name }}
-              <v-chip
-                medium
-                label
-                :color="
-                  $vuetify.theme.dark
-                    ? ''
-                    : permit.isComplete
-                    ? 'primary'
-                    : 'error'
-                "
-                class="ml-4 white--text"
-              >
-                {{ permit.isComplete ? 'Ready for review' : 'Incomplete' }}
-              </v-chip>
+      <v-data-table
+        :items="assignedApplications"
+        :headers="headers"
+        :search="search"
+        :items-per-page="5"
+      >
+        <template #[`item.name`]="{ item }">
+          <router-link
+            :to="{
+              name: 'PermitDetail',
+              params: { orderId: item.orderId },
+            }"
+            tag="a"
+            target="_self"
+            style="text-decoration: none; color: inherit"
+            replace
+          >
+            {{ item.name }}
+          </router-link>
+        </template>
 
-              <v-chip
-                medium
-                label
-                color="primary"
-                class="ml-4"
-              >
-                {{ permit.orderId }}
-              </v-chip>
-            </v-col>
-          </v-row>
-        </router-link>
-      </template>
+        <template #[`item.status`]="{ item }">
+          <router-link
+            :to="{
+              name: 'PermitDetail',
+              params: { orderId: item.orderId },
+            }"
+            tag="a"
+            target="_self"
+            style="text-decoration: none; color: inherit"
+            replace
+          >
+            {{ ApplicationStatus[item.status] }}
+          </router-link>
+        </template>
+
+        <template #[`item.appointmentStatus`]="{ item }">
+          <router-link
+            :to="{
+              name: 'PermitDetail',
+              params: { orderId: item.orderId },
+            }"
+            tag="a"
+            target="_self"
+            style="text-decoration: none; color: inherit"
+            replace
+          >
+            {{ AppointmentStatus[item.appointmentStatus] }}
+          </router-link>
+        </template>
+      </v-data-table>
     </v-card-text>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useAuthStore } from '@shared-ui/stores/auth'
 import { usePermitsStore } from '@core-admin/stores/permitsStore'
+import {
+  ApplicationStatus,
+  AppointmentStatus,
+} from '@shared-utils/types/defaultTypes'
+import { computed, ref } from 'vue'
 
 const authStore = useAuthStore()
 const permitsStore = usePermitsStore()
+const search = ref('')
+const headers = [
+  {
+    text: 'Name',
+    value: 'name',
+  },
+  {
+    text: 'Status',
+    value: 'status',
+  },
+  { text: 'Appt', value: 'appointmentStatus' },
+]
 
 const assignedApplications = computed(() => {
   if (permitsStore.permits) {
