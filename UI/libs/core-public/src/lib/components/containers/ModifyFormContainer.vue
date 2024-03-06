4c843c70f511e033b57650ccc26ce4c2dc4b94f8
Jake Kellas <jacob.kellas@sdsheriff.org>
2024-03-06 10:17:03 -0800

    feat: add stepper

--- UI/libs/core-public/src/lib/components/containers/ModifyFormContainer.vue
+++ UI/libs/core-public/src/lib/components/containers/ModifyFormContainer.vue
@@ -1,36 +1,149 @@
 <template>
-  <v-card :loading="isLoading">
-    <v-card-title>Modification</v-card-title>
+  <div>
+    <v-container
+      v-if="!$vuetify.breakpoint.mdAndDown"
+      fluid
+    >
+      <v-stepper
+        v-model="stepIndex.step"
+        non-linear
+        class="stepper"
+        @change="updateMutation"
+        :alt-labels="$vuetify.breakpoint.lgAndDown"
+      >
+        <v-stepper-header
+          :class="$vuetify.theme.dark ? 'sticky-dark' : 'sticky-light'"
+        >
+          <v-stepper-step
+            editable
+            :complete="stepOneValid"
+            :edit-icon="stepOneValid ? 'mdi-check' : '$edit'"
+            :color="stepOneValid ? 'success' : 'primary'"
+            :step="1"
+          >
+            <div class="text-center">{{ $t('Name Change') }}</div>
+          </v-stepper-step>
 
-    <v-card-text>
-      <v-row>
-        <v-col>
-          <v-text-field label="Modify"></v-text-field>
-        </v-col>
+          <v-divider></v-divider>
 
-        <v-col>
-          <v-text-field label="Modify"></v-text-field>
-        </v-col>
-      </v-row>
-    </v-card-text>
-  </v-card>
+          <v-stepper-step
+            editable
+            :complete="stepTwoValid"
+            :edit-icon="stepTwoValid ? 'mdi-check' : '$edit'"
+            :color="stepTwoValid ? 'success' : 'primary'"
+            :step="2"
+          >
+            <div class="text-center">
+              {{ $t('Address Change') }}
+            </div>
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step
+            editable
+            :complete="stepThreeValid"
+            :edit-icon="stepThreeValid ? 'mdi-check' : '$edit'"
+            :color="stepThreeValid ? 'success' : 'primary'"
+            :step="3"
+          >
+            <div class="text-center">
+              {{ $t('Weapon Change') }}
+            </div>
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step
+            editable
+            :complete="stepFourValid"
+            :edit-icon="stepFourValid ? 'mdi-check' : '$edit'"
+            :color="stepFourValid ? 'success' : 'primary'"
+            :step="4"
+          >
+            <div class="text-center">
+              {{ $t('Supporting Documents') }}
+            </div>
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step
+            editable
+            :complete="stepFiveValid"
+            :edit-icon="stepFiveValid ? 'mdi-check' : '$edit'"
+            :color="stepFiveValid ? 'success' : 'primary'"
+            :step="5"
+          >
+            <div class="text-center">
+              {{ $t('Finalize') }}
+            </div>
+          </v-stepper-step>
+
+          <v-progress-linear
+            :active="isGetApplicationLoading || isUpdateApplicationLoading"
+            indeterminate
+          ></v-progress-linear>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content :step="1">
+            <v-card-title>Modify the first thing</v-card-title>
+          </v-stepper-content>
+        </v-stepper-items>
+
+        <v-stepper-items>
+          <v-stepper-content :step="2">
+            <v-card-title>Modify the second thing</v-card-title>
+          </v-stepper-content>
+        </v-stepper-items>
+
+        <v-stepper-items>
+          <v-stepper-content :step="3">
+            <v-card-title>Modify the third thing</v-card-title>
+          </v-stepper-content>
+        </v-stepper-items>
+
+        <v-stepper-items>
+          <v-stepper-content :step="4">
+            <v-card-title>Modify the fourth thing</v-card-title>
+          </v-stepper-content>
+        </v-stepper-items>
+
+        <v-stepper-items>
+          <v-stepper-content :step="5">
+            <v-card-title>Modify the fifth thing</v-card-title>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { CompleteApplication } from '@shared-utils/types/defaultTypes'
 import { useCompleteApplicationStore } from '@shared-ui/stores/completeApplication'
-import { useQuery } from '@tanstack/vue-query'
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
+import { useMutation, useQuery } from '@tanstack/vue-query'
 
 const applicationStore = useCompleteApplicationStore()
 const isApplicationValid = ref(false)
+const stepIndex = reactive({
+  step: 1,
+  previousStep: 1,
+})
+const stepOneValid = ref(false)
+const stepTwoValid = ref(false)
+const stepThreeValid = ref(false)
+const stepFourValid = ref(false)
+const stepFiveValid = ref(false)
 
 onMounted(() => {
   window.scrollTo(0, 0)
   isApplicationValid.value = Boolean(applicationStore.completeApplication.id)
 })
 
-const { isLoading } = useQuery(
+const { isLoading: isGetApplicationLoading } = useQuery(
   ['getApplicationsByUser'],
   () => applicationStore.getAllUserApplicationsApi(),
   {
@@ -40,4 +153,51 @@ const { isLoading } = useQuery(
     },
   }
 )
+
+const { isLoading: isUpdateApplicationLoading, mutate: updateMutation } =
+  useMutation({
+    mutationFn: () => {
+      return applicationStore.updateApplication()
+    },
+  })
 </script>
+
+<style lang="scss">
+.theme--dark.v-expansion-panels .v-expansion-panel {
+  background: #303030;
+}
+
+.v-expansion-panel-content__wrap {
+  padding-left: 5px !important;
+  padding-right: 5px !important;
+}
+
+.stepper {
+  overflow: visible;
+}
+
+.sticky-light {
+  position: sticky;
+  top: 64px;
+  z-index: 1;
+  background-color: white;
+}
+
+.sticky-dark {
+  position: sticky;
+  top: 64px;
+  z-index: 1;
+  background-color: #303030;
+}
+
+.progress-circular {
+  position: fixed;
+  top: 75px;
+  left: 50%;
+  z-index: 2;
+}
+
+.theme--dark.v-label.v-label--active {
+  color: white !important;
+}
+</style>
