b4d35c508ace4c53018b824cd690ec733e3e988e
winnietho <142354721+winnietho@users.noreply.github.com>
2024-04-30 10:15:40 -0700

    feat: finish confirmation dialog

--- UI/libs/core-admin/src/lib/components/dialogs/AppointmentActionConfirmationDialog.vue
+++ UI/libs/core-admin/src/lib/components/dialogs/AppointmentActionConfirmationDialog.vue
@@ -4,7 +4,10 @@
     width="500"
   >
     <template #activator="{ on: dialogActivator, attrs }">
-      <v-tooltip bottom>
+      <v-tooltip
+        v-if="props.checkIn"
+        bottom
+      >
         <template #activator="{ on: tooltip }">
           <v-btn
             v-if="!props.undoActive"
@@ -16,22 +19,25 @@
           >
             <v-icon> mdi-check-bold </v-icon>
           </v-btn>
-          <!-- <v-btn
-          v-else
-          v-bind="attrs"
-          v-on="on"
-          color="success"
-          class="mr-2"
-          icon
-        >
-          <v-icon> mdi-undo </v-icon>
-        </v-btn> -->
+          <v-btn
+            v-else
+            v-bind="attrs"
+            v-on="{ ...tooltip, ...dialogActivator }"
+            color="success"
+            class="mr-2"
+            icon
+          >
+            <v-icon> mdi-undo </v-icon>
+          </v-btn>
         </template>
         <span v-if="!props.undoActive"> Check In </span>
-        <span v-else>Undo Check In</span>
+        <span v-else> Undo Check In </span>
       </v-tooltip>
-  
-      <v-tooltip bottom>
+
+      <v-tooltip
+        bottom
+        v-else
+      >
         <template #activator="{ on: tooltip }">
           <v-btn
             v-if="!props.undoActive"
@@ -43,55 +49,38 @@
           >
             <v-icon> mdi-close-thick </v-icon>
           </v-btn>
-          <!-- <v-btn
-          v-else
-          v-bind="attrs"
-          v-on="on"
-          color="success"
-          class="mr-2"
-          icon
-        >
-          <v-icon> mdi-undo </v-icon>
-        </v-btn> -->
+          <v-btn
+            v-else
+            v-bind="attrs"
+            v-on="{ ...tooltip, ...dialogActivator }"
+            color="error"
+            class="mr-2"
+            icon
+          >
+            <v-icon> mdi-undo </v-icon>
+          </v-btn>
         </template>
         <span v-if="!props.undoActive"> No Show </span>
-        <span v-else>Undo No Show</span>
+        <span v-else> Undo No Show </span>
       </v-tooltip>
     </template>
-    <!-- <v-tooltip bottom>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          v-if="!props.undoActive"
-          v-bind="attrs"
-          v-on="on"
-          color="error"
-          class="mr-2"
-          icon
-        >
-          <v-icon> mdi-close-thick </v-icon>
-        </v-btn>
-        <v-btn
-          v-else
-          v-bind="attrs"
-          v-on="on"
-          color="error"
-          class="mr-2"
-          icon
-        >
-          <v-icon>mdi-undo</v-icon>
-        </v-btn>
-      </template>
-      <span v-if="!props.undoActive">No Show</span>
-      <span v-else>Undo No Show</span>
-    </v-tooltip> -->
 
     <v-card>
       <v-card-title> {{ getTitle }} </v-card-title>
-
-      <v-card-text> template test </v-card-text>
-
+      <v-card-text>
+        Are you sure you want to select {{ getTitle.toLowerCase() }}?
+      </v-card-text>
       <v-card-actions>
         <v-btn
+          color="primary"
+          text
+          @click="cancelDialog"
+        >
+          Cancel
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn
+          @click="handleConfirm"
           color="primary"
           text
         >
@@ -112,10 +101,24 @@ interface AppointmentActionConfirmationDialog {
 }
 
 const props = defineProps<AppointmentActionConfirmationDialog>()
-
+const emit = defineEmits(['confirm', 'undo'])
 const dialog = ref(false)
 
 const getTitle = computed(() => {
   return props.undoActive ? `Undo ${props.title}` : props.title
 })
+
+function cancelDialog() {
+  dialog.value = false
+}
+
+function handleConfirm() {
+  if (props.undoActive) {
+    emit('undo')
+  } else {
+    emit('confirm')
+  }
+
+  dialog.value = false
+}
 </script>
