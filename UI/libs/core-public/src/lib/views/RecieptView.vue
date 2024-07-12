f56552462dd93be6172659b43810051a5b08ae32
winnietho <142354721+winnietho@users.noreply.github.com>
2024-07-12 15:30:55 -0700

    fix: add vsheet, remove css

--- UI/libs/core-public/src/lib/views/RecieptView.vue
+++ UI/libs/core-public/src/lib/views/RecieptView.vue
@@ -1,116 +1,126 @@
 <template>
-  <v-container
-    class="d-flex align-center justify-center"
-    style="height: 75vh"
-  >
+  <v-container class="align-center justify-center">
     <v-row
       justify="center"
       align="center"
     >
-      <v-col
-        cols="12"
-        sm="8"
-        md="6"
-      >
-        <v-card-title class="justify-center">
-          <v-icon
-            large
-            color="success"
-          >
-            mdi-check-circle
-          </v-icon>
-        </v-card-title>
-        <v-card-text class="text-center">
-          <h2>Your application has been submitted!</h2>
-          <p class="subtitle-1 mt-7">
-            Thank you for completing your CCW application. The Licensing Staff
-            will see you at your appointment.
-          </p>
-          <v-row><v-col></v-col></v-row>
-          <v-row>
-            <v-col
-              cols="5"
-              class="mt-12 mb-4"
+      <v-col>
+        <v-card
+          flat
+          height="700px"
+        >
+          <v-card-title class="justify-center mt-10">
+            <v-icon
+              large
+              color="success"
             >
-              <v-alert
-                type="info"
-                outlined
-                class="custom-alert"
+              mdi-check-circle
+            </v-icon>
+          </v-card-title>
+          <v-card-text class="text-center mb-12">
+            <h2
+              class="mb-10"
+              style="color: black"
+              :class="themeStore.getThemeConfig.isDark ? 'white--text' : ''"
+            >
+              Your application has been submitted!
+            </h2>
+            <p
+              class="subtitle-1"
+              style="color: black"
+              :class="themeStore.getThemeConfig.isDark ? 'white--text' : ''"
+            >
+              Thank you for completing your CCW application. The Licensing Staff
+              will see you at your appointment.
+            </p>
+            <v-row><v-col></v-col></v-row>
+            <v-row justify="center">
+              <v-col
+                cols="5"
+                class="mr-5"
               >
-                Please read the following:
-              </v-alert>
+                <v-sheet
+                  outlined
+                  rounded
+                  color="info"
+                  class="ml-6"
+                >
+                  <v-card flat>
+                    <v-card-title class="justify-center">
+                      <v-icon
+                        left
+                        color="blue"
+                        >mdi-information</v-icon
+                      >
+                      <v-span
+                        text="info"
+                        :class="
+                          themeStore.getThemeConfig.isDark ? 'white--text' : ''
+                        "
+                        >Please read the following:
+                      </v-span>
+                    </v-card-title>
+                    <v-card-text>
+                      <ol
+                        class="mb-2"
+                        style="color: black; text-align: left"
+                        :class="
+                          themeStore.getThemeConfig.isDark ? 'white--text' : ''
+                        "
+                      >
+                        <li style="font-size: 0.975rem">
+                          If any of your contact information
+                          <strong>changes</strong>, please log in and
+                          <strong>update</strong> your application
+                          <strong>immediately</strong>.
+                        </li>
+                        <li style="font-size: 0.975rem">
+                          You can view the <strong>status</strong> of your
+                          application by logging in and pressing the
+                          <strong>"View Application"</strong> button.
+                        </li>
+                        <li style="font-size: 0.975rem">
+                          You will be <strong>notified</strong> when
+                          <strong>payment</strong> is due.
+                        </li>
+                      </ol>
+                    </v-card-text>
+                  </v-card>
+                </v-sheet>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-row>
+            <v-col>
+              <v-card-text class="left">
+                <v-row justify="center">
+                  <v-btn
+                    color="primary"
+                    @click="goToDashBoard"
+                  >
+                    Back To Home
+                  </v-btn>
+                </v-row>
+              </v-card-text>
             </v-col>
           </v-row>
-        </v-card-text>
-        <v-row justify="center">
-          <v-col
-            cols="12"
-            sm="11"
-            offset-sm="0"
-          >
-            <v-card-text
-              class="left ml-2"
-              style="line-height: 2.5"
-            >
-              <ol>
-                <li style="font-size: 0.975rem">
-                  If any of your contact information <strong>changes</strong>,
-                  please log in and <strong>update</strong> your application
-                  <strong>immediately</strong>.
-                </li>
-                <li style="font-size: 0.975rem">
-                  You can view the <strong>status</strong> of your application
-                  by logging in and pressing the
-                  <strong>"View Application"</strong> button.
-                </li>
-                <li style="font-size: 0.975rem">
-                  You will be <strong>notified</strong> when
-                  <strong>payment</strong> is due.
-                </li>
-              </ol>
-              <v-row justify="center">
-                <v-btn
-                  justify="center"
-                  color="primary"
-                  class="mt-15"
-                  @click="goToDashBoard"
-                >
-                  Back To Home
-                </v-btn>
-              </v-row>
-            </v-card-text>
-          </v-col>
-        </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { useThemeStore } from '@shared-ui/stores/themeStore'
 import { useBrandStore } from '@shared-ui/stores/brandStore'
 import { useRouter } from 'vue-router/composables'
 
 const router = useRouter()
 const brandStore = useBrandStore()
+const themeStore = useThemeStore()
 
 const goToDashBoard = () => {
   router.push('/')
 }
 </script>
-<style lang="scss">
-.v-alert {
-  position: fixed;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: 0 auto;
-}
-
-.custom-alert {
-  height: 50px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-}
-</style>
+<style lang="scss"></style>
