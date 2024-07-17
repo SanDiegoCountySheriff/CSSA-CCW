19ddef76a8a59facadfc178d1bc2dc2e0b5dfa5a
Nate Wolford <104584843+nwolfosh@users.noreply.github.com>
2024-07-17 06:56:38 -0700

    Update IEmailService.cs

--- API/CCW.Common/Services/Contracts/IEmailService.cs
+++ API/CCW.Common/Services/Contracts/IEmailService.cs
@@ -1,9 +1,4 @@
 using CCW.Common.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CCW.Common.Services.Contracts
 {
