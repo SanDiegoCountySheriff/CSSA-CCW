ed3f50967e85e7277980937df95a8571227050fe
Jake Kellas <jacob.kellas@sdsheriff.org>
2024-07-31 12:36:15 -0700

    fix: appointment breaks and end times

--- API/CCW.Schedule/Services/AppointmentCosmosDbService.cs
+++ API/CCW.Schedule/Services/AppointmentCosmosDbService.cs
@@ -537,8 +537,8 @@ public class AppointmentCosmosDbService : IAppointmentCosmosDbService
                 {
                     if (appointmentManagement.BreakStartTime != null && WillAppointmentFallInBreakTime(appointmentManagement, startTime))
                     {
-                        startTime = startTime.Add(new TimeSpan(0, appointmentManagement.AppointmentLength, 0));
-                        endTime = endTime.Add(new TimeSpan(0, appointmentManagement.AppointmentLength, 0));
+                        startTime = startTime.Add(new TimeSpan(0, (int)appointmentManagement.BreakLength, 0));
+                        endTime = endTime.Add(new TimeSpan(0, (int)appointmentManagement.BreakLength, 0));
                         continue;
                     }
 
@@ -566,17 +566,18 @@ public class AppointmentCosmosDbService : IAppointmentCosmosDbService
                             offset
                         );
 
-                        var endHours = startTime.Minutes == 30 ? startTime.Hours + 1 : startTime.Hours;
-
+                        var endHours = startTime.Hours;
                         var endMinutes = startTime.Minutes + appointmentManagement.AppointmentLength;
 
                         if (endMinutes == 60)
                         {
                             endMinutes = 0;
+                            endHours += 1;
                         }
                         else if (endMinutes > 60)
                         {
                             endMinutes -= 60;
+                            endHours += 1;
                         }
 
                         var correctedAppointmentEnd = new DateTimeOffset(
